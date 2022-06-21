c10dddc72849b200a6aaca46bd063c2550e7804e
hernanguaragna <hernanguaragna@gmail.com>
2022-06-21 16:53:06 -0300

    orodenada la navegacion

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import {dirname, join} from "path";
 import { fileURLToPath } from "url";
 
+import indexRouter from "./routes/index.js";
+
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -10,12 +12,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set("views", join(__dirname, 'views'));
 app.set("view engine", "ejs");	// set the view engine to ejs
 
+app.use(indexRouter);// use the index router for all requests(gets)
 
-app.get ("/", (req, res) => res.render("index"));
 
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
     }   
 );  // listen on port 3000, default is 3000
-//QUEDE EN MITUNO 18.50 DE TU PRIMER APLICACION DE NODEJS DESDE CERO
