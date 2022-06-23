ab6a1aaa705b57191e8ec2fdbf30b34c962fc230
hernanguaragna <hernanguaragna@gmail.com>
2022-06-23 16:22:46 -0300

    modificado para que ande en heroku

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(indexRouter);// use the index router for all requests(gets)
 app.use(express.static(join(__dirname, 'public'))); // use the public folder for static files
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on port 3000", process.env.PORT);
     }   
-);  // listen on port 3000, default is 3000
+);  // listen on port 3000, default is 3000 y para que funcione en heroku procees.env.PORT(variable de entorno)
